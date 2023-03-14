9c7e3cc834a6b4189d7c088f49ab80d9b473d38c
HoangPhiem <tuhoangphiem10@gmail.com>
2023-03-14 17:26:16 +0700

    [LOC-0] Set up project for backend

--- server/src/app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-
-const app = express()
-const port = process.env.PORT
-
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`)
-});
